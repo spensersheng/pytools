f14907f409c0e72f16526b15dc1a7089d1c4cd57
Spenser Sheng <seieipax@163.com>
2014-11-27 14:08:13 +0800

    added gitignore file

---
+++ mockssh/linux/commands.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#Autohor: Spenser Sheng
+
+
+import MockSSH
+
+
+#
+# command: exit
+#
+def exit_command_success(instance):
+    if 'config' in instance.protocol.prompt:
+        instance.protocol.prompt = "hostname#"
+    else:
+        instance.protocol.call_command(
+            instance.protocol.commands['_exit'])
+
+
+def exit_command_failure(instance):
+    instance.writeln("MockSSH: supported usage: exit")
+
+
+def getcmd_exit():
+    command_exit = MockSSH.ArgumentValidatingCommand(
+        'exit',
+        [exit_command_success],
+        [exit_command_failure],
+        *[])
+    return command_exit
